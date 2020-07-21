3d3a101a20deeb1780ddf623a8968af55126acf7
Aurélien Baumann <aur.baumann@gmail.com>
2020-07-21 14:28:25 +0200

    add some code comments

--- app.php
+++ app.php
@@ -11,28 +11,40 @@ $nbEntriesPerMessage = getenv('NB_ENTRIES_PER_MESSAGE') ?: 900;
 $pubSub = new PubSubClient(['projectId' => $googleProjectId]);
 $subscription = $pubSub->subscription($googlePubSubSubscription);
 
+// Ticks are needed.
+// https://www.php.net/manual/fr/internals2.opcodes.ticks.php
 declare(ticks=1);
-$continue = true;
 
+// Intercepts SIGINT & SIGTERM signals and sets $continue to false
+// in order to exit the infinite control loop.
+$continue = true;
 function sig_handle()
 {
     global $continue;
     $continue = false;
 }
-
 pcntl_signal(SIGINT, 'sig_handle');
 pcntl_signal(SIGTERM, 'sig_handle');
 
 while ($continue) {
+    // Only pull one message at a time to avoid duplicates.
+    // https://cloud.google.com/pubsub/docs/pull#dupes
     $messages = $subscription->pull(['maxMessages' => 1]);
 
     foreach ($messages as $message) {
         echo "Message id: " . $message->id() . PHP_EOL;
         for ($i = 1; $i <= $nbEntriesPerMessage; $i++) {
             echo "Dealing next entry $i of message " . $message->data() . " " . PHP_EOL;
+
+            // Emulate some work.
             sleep(1);
+
+            // Regularly reset the acknowledgement deadline to avoid duplicates.
+            // https://cloud.google.com/pubsub/docs/pull#dupes
             $subscription->modifyAckDeadline($message, 10);
         }
+
+        // Acknowledge the message and remove it from the Pub/Sub subscription.
         $subscription->acknowledge($message);
         echo "Done dealing with message id: " . $message->id() . PHP_EOL;
     }
